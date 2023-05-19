a15e29cd1abc4725837da7b40bb596fb5f32a8b9
AlexisRios1316 <uranga127@gmail.com>
2023-05-19 15:41:48 -0600

    Se agrego la configuracion notenv

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express'); // imports en node js sin ninguna configuracion 
  const app  = express();
+ const dotenv = require('dotenv'); // Importamos dotenv
  const port = process.env.PORT || 3000;
- 
+
  //Configuracion para recibir inofmracion 
  app.use(express.json()) //´Permite que la peticion post reciba informacion
 
