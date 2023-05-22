aa67bdab6e110f3aa978356f7397995d98ee3a2f
AlexisRios1316 <uranga127@gmail.com>
2023-05-22 13:28:23 -0600

    Cambios generados

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # ProyectoMongo
 Creacion de un Crew con Mongooss
+
+/models
+son todos los modelos de las bases de datos de Mongo DB
+
+/Controllers
+Todos los verbos del modelo
+/UserModel = usercontrolador 
--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const express = require('express'); // imports en node js sin ninguna configuracion 
  const app  = express();
- const dotenv = require('dotenv'); // Importamos dotenv
+ const mongoose = require('mongoose');  //Para realizar la conexion al mongoose 
+ require('dotenv').config() // Importamos dotenv
  const port = process.env.PORT || 3000;
 
  //Configuracion para recibir inofmracion 
  app.use(express.json()) //´Permite que la peticion post reciba informacion
+
+//Conexion a Mongoose 
+mongoose.connect(process.env.HOSTDB).then(() => {
+console.log('Conexion a mongo DB')
+}).catch((error) => {
+    console.log(error)
+})
 
 
 // Servidor 
